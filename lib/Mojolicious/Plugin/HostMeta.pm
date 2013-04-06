f437d1451f1e85d4cab7dae51fb00a0f7fdb7462
Nils Diewald <akron@cpan.org>
2013-04-06 22:16:49 +0000

    Import of AKRON/Mojolicious-Plugin-HostMeta-0.04 from CPAN.
    gitpan-cpan-distribution: Mojolicious-Plugin-HostMeta
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         AKRON/Mojolicious-Plugin-HostMeta-0.04.tar.gz
    gitpan-cpan-author:       AKRON
    gitpan-cpan-maturity:     released

---
+++ lib/Mojolicious/Plugin/HostMeta.pm
@@ -0,0 +1,514 @@
+package Mojolicious::Plugin::HostMeta;
+use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::UserAgent;
+use Mojo::Headers;
+use Mojo::JSON;
+use Mojo::Util qw/quote/;
+use Mojo::IOLoop;
+
+
+our $VERSION = 0.04;
+
+
+my $WK_PATH = '/.well-known/host-meta';
+my $UA_NAME = __PACKAGE__ . ' v' . $VERSION;
+
+
+# Register plugin
+sub register {
+  my ($plugin, $mojo, $param) = @_;
+
+  $param ||= {};
+
+  # Load parameter from Config file
+  if (my $config_param = $mojo->config('HostMeta')) {
+    $param = { %$config_param, %$param };
+  };
+
+  # Get helpers object
+  my $helpers = $mojo->renderer->helpers;
+
+  # Load Util-Endpoint/Callback if not already loaded
+  foreach (qw/Endpoint Callback/) {
+    $mojo->plugin("Util::$_") unless exists $helpers->{ lc $_ };
+  };
+
+  # Set callbacks on registration
+  $mojo->callback(['hostmeta_fetch'] => $param);
+
+  # Load XML if not already loaded
+  unless (exists $helpers->{render_xrd}) {
+    $mojo->plugin('XRD');
+  };
+
+  # Get seconds to expiration
+  my $seconds = (60 * 60 * 24 * 10);
+  if ($param->{expires} && $param->{expires} =~ /^\d+$/) {
+    $seconds = delete $param->{expires};
+  };
+
+  # Create new hostmeta document
+  my $hostmeta = $mojo->new_xrd;
+  $hostmeta->extension( -HostMeta );
+
+  # Get host information on first request
+  $mojo->hook(
+    prepare_hostmeta =>
+      sub {
+	my ($c, $xrd_ref) = @_;
+	my $host = $c->req->url->to_abs->host;
+
+	# Add host-information to host-meta
+	$hostmeta->host( $host ) if $host;
+      }
+    );
+
+  # Establish 'hostmeta' helper
+  $mojo->helper(
+    hostmeta => sub {
+      my $c = shift;
+
+      # Undefined host name
+      shift if !defined $_[0];
+
+      # Host name is provided
+      if (!$_[0] || ref $_[0]) {
+
+	# Return local hostmeta
+	return _serve_hostmeta( $c, $hostmeta, @_ );
+      };
+
+      # Return discovered hostmeta
+      return _fetch_hostmeta( $c, @_ );
+    });
+
+  # Establish /.well-known/host-meta route
+  my $route = $mojo->routes->route( $WK_PATH );
+
+  # Define endpoint
+  $route->endpoint('host-meta');
+
+  # Set route callback
+  $route->to(
+    cb => sub {
+      my $c = shift;
+
+      # Seconds given
+      if ($seconds) {
+
+	# Set cache control
+	my $headers = $c->res->headers;
+	$headers->cache_control(
+	  "public, max-age=$seconds"
+	);
+
+	# Set expires element
+	$hostmeta->expires( time + $seconds );
+
+	# Set expires header
+	$headers->expires( $hostmeta->expires );
+      };
+
+      # Serve host-meta document
+      return $c->render_xrd(
+	_serve_hostmeta( $c, $hostmeta )
+      );
+    });
+};
+
+
+# Get HostMeta document
+sub _fetch_hostmeta {
+  my $c    = shift;
+  my $host = lc shift;
+
+  # Get headers
+  my $header = {};
+  if ($_[0] && ref $_[0] && ref($_[0]) eq 'HASH') {
+    $header = shift;
+  };
+
+  # Check if security is forced
+  my $secure = $_[-1] && $_[-1] eq '-secure' ? pop : 0;
+
+  # Get callback
+  my $cb = pop if ref($_[-1]) && ref($_[-1]) eq 'CODE';
+
+  # Get host information
+  unless ($host =~ s!^\s*(?:http(s?)://)?([^/]+)/*\s*$!$2!) {
+    return;
+  };
+  $secure = 1 if $1;
+
+  # Build relations parameter
+  my $rel;
+  $rel = shift if $_[0] && ref($_[0]) eq 'ARRAY';
+
+  # Callback for caching
+  my ($xrd, $headers) = $c->callback(
+    hostmeta_fetch => $host
+  );
+
+  # HostMeta document was cached
+  if ($xrd) {
+
+    # Filter relations
+    $xrd = $xrd->filter_rel( $rel ) if $rel;
+
+    # Set headers to default
+    $headers ||= Mojo::Headers->new if $cb || wantarray;
+
+    # Return cached hostmeta document
+    return $cb->( $xrd, $headers ) if $cb;
+    return ( $xrd, $headers ) if wantarray;
+    return $xrd;
+  };
+
+  # Create host-meta path
+  my $path = '//' . $host . $WK_PATH;
+  $path = 'https:' . $path if $secure;
+
+
+  # Non-blocking
+  if ($cb) {
+    return $c->get_xrd(
+      $path => $header => sub {
+	my ($xrd, $headers) = @_;
+	if ($xrd) {
+
+	  # Add hostmeta extension
+	  $xrd->extension(-HostMeta);
+
+	  # Hook for caching
+	  $c->app->plugins->emit_hook(
+	    after_fetching_hostmeta => (
+	      $c, $host, $xrd, $headers
+	    )
+	  );
+
+	  # Filter based on relations
+	  $xrd = $xrd->filter_rel( $rel ) if $rel;
+
+	  # Send to callback
+	  return $cb->( $xrd, $headers );
+	};
+
+	# Fail
+	return $cb->();
+      });
+  };
+
+  # Blocking
+  ($xrd, $headers) = $c->get_xrd( $path => $header );
+
+  # No host-meta found
+  return unless $xrd;
+
+  # Add hostmeta extension
+  $xrd->extension( -HostMeta );
+
+  # Hook for caching
+  $c->app->plugins->emit_hook(
+    after_fetching_hostmeta => (
+      $c, $host, $xrd, $headers
+    )
+  );
+
+  # Filter based on relations
+  $xrd = $xrd->filter_rel( $rel ) if $rel;
+
+  # Return
+  return ($xrd, $headers) if wantarray;
+  return $xrd;
+};
+
+
+# Run hooks for preparation and serving of hostmeta
+sub _serve_hostmeta {
+  my $c   = shift;
+  my $xrd = shift;
+
+  # Ignore security flag
+  pop if $_[-1] && $_[-1] eq '-secure';
+
+  # Ignore header information
+  shift if $_[0] && ref($_[0]) && ref($_[0]) eq 'HASH';
+
+  # Get callback
+  my $cb = pop if ref($_[-1]) && ref($_[-1]) eq 'CODE';
+
+  my $rel = shift;
+
+  my $plugins = $c->app->plugins;
+  my $phm = 'prepare_hostmeta';
+
+  # prepare_hostmeta has subscribers
+  if ($plugins->has_subscribers( $phm )) {
+
+    # Emit hook for subscribers
+    $plugins->emit_hook( $phm => ( $c, $xrd ));
+
+    # Unsubscribe all subscribers
+    foreach (@{ $plugins->subscribers( $phm ) }) {
+      $plugins->unsubscribe( $phm => $_ );
+    };
+  };
+
+  # No further modifications wanted
+  unless ($plugins->has_subscribers('before_serving_hostmeta')) {
+
+    # Filter relations
+    $xrd = $xrd->filter_rel( $rel ) if $rel;
+
+    # Return document
+    return $cb->( $xrd ) if $cb;
+    return $xrd;
+  };
+
+  # Clone hostmeta reference
+  $xrd = $c->new_xrd( $xrd->to_xml );
+
+  # Emit 'before_serving_hostmeta' hook
+  $plugins->emit_hook(
+    before_serving_hostmeta => (
+      $c, $xrd
+    ));
+
+  # Filter relations
+  $xrd = $xrd->filter_rel( $rel ) if $rel;
+
+  # Return hostmeta clone
+  return $cb->( $xrd ) if $cb;
+  return $xrd;
+};
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Mojolicious::Plugin::HostMeta - Serve and Retrieve Host-Meta documents
+
+
+=head1 SYNOPSIS
+
+  # Mojolicious
+  $self->plugin('HostMeta');
+
+  # Mojolicious::Lite
+  plugin 'HostMeta';
+
+  # Serves XRD or JRD from /.well-known/host-meta
+
+  # Blocking requests
+  print $self->hostmeta('gmail.com')->link('lrrd');
+
+  # Non-blocking requests
+  $self->hostmeta('gmail.com' => sub {
+    print shift->link('lrrd');
+  });
+
+
+=head1 DESCRIPTION
+
+L<Mojolicious::Plugin::HostMeta> is a Mojolicious plugin to serve and
+request "well-known" L<Host-Meta|https://tools.ietf.org/html/rfc6415>
+documents.
+
+
+=head1 METHODS
+
+=head2 C<register>
+
+  # Mojolicious
+  $app->plugin(HostMeta => {
+    expires => 100
+  });
+
+  # Mojolicious::Lite
+  plugin 'HostMeta';
+
+Called when registering the plugin.
+Accepts one optional parameter C<expires>, which is the number
+of seconds the served host-meta should be cached by the fetching client.
+Defaults to 10 days.
+This parameter can be set either on registration or
+as part of the configuration file with the key C<HostMeta>.
+
+
+=head1 HELPERS
+
+=head2 C<hostmeta>
+
+  # In Controller:
+  my $xrd = $self->hostmeta;
+  $xrd = $self->hostmeta('gmail.com');
+  $xrd = $self->hostmeta('sojolicio.us' => ['hub']);
+  $xrd = $self->hostmeta('sojolicio.us', { 'X-MyHeader' => 'Fun' } => ['hub']);
+  $xrd = $self->hostmeta('gmail.com', -secure);
+
+  # Non blocking
+  $self->hostmeta('gmail.com' => ['hub'] => sub {
+    my $xrd = shift;
+    # ...
+  }, -secure);
+
+This helper returns host-meta documents
+as L<XML::Loy::XRD> objects with the
+L<XML::Loy::HostMeta> extension.
+
+If no host name is given, the local host-meta document is returned.
+If a host name is given, the corresponding host-meta document
+is retrieved from the host and returned.
+
+An additional hash reference or a L<Mojo::Headers> object can be used
+to pass header information for retrieval.
+An additional array reference may limit the relations to be retrieved
+(see the L<WebFinger|http://tools.ietf.org/html/draft-ietf-appsawg-webfinger>
+specification for further explanation).
+A final C<-secure> flag indicates, that discovery is allowed
+only over C<https> without redirections.
+
+This method can be used in a blocking or non-blocking way.
+For non-blocking retrieval, pass a callback function as the
+last argument before the optional C<-secure> flag to the method.
+
+
+=head1 CALLBACKS
+
+=head2 hostmeta_fetch
+
+  # Establish a callback
+  $mojo->callback(
+    hostmeta_fetch => sub {
+      my ($c, $host) = @_;
+
+      my $doc = $c->chi->get("hostmeta-$host");
+      return unless $doc;
+
+      my $header = $c->chi->get("hostmeta-$host-headers");
+
+      # Return document
+      return ($c->new_xrd($doc), Mojo::Headers->new->parse($header));
+    }
+  );
+
+This callback is released before a host-meta document
+is retrieved from a foreign server. The parameters passed to the
+callback include the current controller object and the host's
+name.
+
+If a L<XML::Loy::XRD> document associated with the requested
+host name is returned (and optionally a L<Mojo::Headers> object),
+the retrieval will stop.
+
+The callback can be established with the
+L<callback|Mojolicious::Plugin::Util::Callback/callback>
+helper or on registration.
+
+This can be used for caching.
+
+
+=head1 HOOKS
+
+=head2 prepare_hostmeta
+
+  $mojo->hook(prepare_hostmeta => sub {
+    my ($c, $hostmeta) = @_;
+    $hostmeta->link(permanent => '/perma.html');
+  };
+
+This hook is run when the host's own host-meta document is
+first prepared. The hook passes the current controller
+object and the host-meta document as an L<XML::Loy::XRD> object.
+This hook is only emitted once for each subscriber.
+
+
+=head2 before_serving_hostmeta
+
+  $mojo->hook('before_serving_hostmeta' => sub {
+    my ($c, $hostmeta) = @_;
+    $hostmeta->link(lrdd => './well-known/host-meta');
+  };
+
+This hook is run before the host's own host-meta document is
+served. The hook passes the current controller object and
+the host-meta document as an L<XML::Loy::XRD> object.
+This should be used for dynamical changes of the document
+for each request.
+
+
+=head2 after_fetching_hostmeta
+
+  $mojo->hook(
+    after_fetching_hostmeta => sub {
+      my ($c, $host, $xrd, $headers) = @_;
+
+      # Store in cache
+      my $chi = $c->chi;
+      $chi->set("hostmeta-$host" => $xrd->to_xml);
+      $chi->set("hostmeta-$host-headers" => $headers->to_string);
+    }
+  );
+
+This hook is run after a foreign host-meta document is newly fetched.
+The parameters passed to the hook are the current controller object,
+the host name, the XRD document as an L<XML::Loy::XRD> object
+and the L<headers|Mojo::Headers> object of the response.
+
+This can be used for caching.
+
+
+=head1 ROUTES
+
+The route C</.well-known/host-meta> is established and serves
+the host's own host-meta document.
+An L<endpoint|Mojolicious::Plugin::Util::Endpoint> called
+C<host-meta> is established.
+
+
+=head1 EXAMPLES
+
+The C<examples/> folder contains a full working example application
+with serving and discovery.
+The example has an additional dependency of L<CHI>.
+
+It can be started using the daemon, morbo or hypnotoad.
+
+  $ perl examples/hostmetaapp daemon
+
+This example may be a good starting point for your own implementation.
+
+A less advanced application using non-blocking requests without caching
+is also available in the C<examples/> folder. It can be started using
+the daemon, morbo or hypnotoad as well.
+
+  $ perl examples/hostmetaapp-async daemon
+
+
+=head1 DEPENDENCIES
+
+L<Mojolicious> (best with SSL support),
+L<Mojolicious::Plugin::Util::Endpoint>,
+L<Mojolicious::Plugin::Util::Callback>,
+L<Mojolicious::Plugin::XRD>.
+
+
+=head1 AVAILABILITY
+
+  https://github.com/Akron/Mojolicious-Plugin-HostMeta
+
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2011-2013, L<Nils Diewald|http://nils-diewald.de/>.
+
+This program is free software, you can redistribute it
+and/or modify it under the same terms as Perl.
+
+=cut
---
+++ t/HostMeta.t
@@ -0,0 +1,264 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use lib ('lib', '../lib');
+
+use Test::More;
+use Test::Mojo;
+use Mojo::JSON;
+use Mojolicious::Lite;
+
+my $hm_host = 'hostme.ta';
+
+my $t = Test::Mojo->new;
+my $app = $t->app;
+$app->plugin('HostMeta');
+
+my $c = Mojolicious::Controller->new;
+
+# Set request information globally
+$c->app($app);
+$c->req->url->base->parse('http://' . $hm_host);
+
+$app->hook(
+  before_dispatch => sub {
+    my $c = shift;
+    my $base = $c->req->url->base;
+    $base->parse('http://' . $hm_host . '/');
+    $base->port('');
+  });
+
+my $h = $app->renderer->helpers;
+
+# XRD
+ok($h->{new_xrd}, 'render_xrd fine.');
+ok($h->{render_xrd}, 'render_xrd fine.');
+
+# Util::Endpoint
+ok($h->{endpoint}, 'endpoint fine.');
+
+# Hostmeta
+ok($h->{hostmeta}, 'hostmeta fine.');
+
+# Complementary check
+ok(!exists $h->{foobar}, 'foobar not fine.');
+
+$t->get_ok('/.well-known/host-meta')
+    ->status_is(200)
+    ->content_type_is('application/xrd+xml')
+    ->element_exists('XRD')
+    ->element_exists('XRD[xmlns]')
+    ->element_exists('XRD[xsi]')
+    ->element_exists_not('Link')
+    ->element_exists_not('Property')
+    ->element_exists('Host')->text_is(Host => $hm_host);
+
+$app->hook(
+  'before_serving_hostmeta' => sub {
+    my ($c, $xrd) = @_;
+
+    # Set property
+    $xrd->property('foo' => 'bar');
+
+    # Check endpoint
+    is($c->endpoint('host-meta'),
+       "http://$hm_host/.well-known/host-meta",
+       'Correct endpoint');
+  });
+
+$t->get_ok('/.well-known/host-meta')
+    ->status_is(200)
+    ->content_type_is('application/xrd+xml')
+    ->element_exists('XRD')
+    ->element_exists('XRD[xmlns]')
+    ->element_exists('XRD[xsi]')
+    ->element_exists_not('Link')
+    ->element_exists('Property')
+    ->element_exists('Property[type="foo"]')
+    ->text_is('Property[type="foo"]' => 'bar')
+    ->element_exists('Host')->text_is(Host => $hm_host);
+
+$app->callback(
+  hostmeta_fetch => sub {
+    my ($c, $host) = @_;
+
+    if ($host eq 'example.org') {
+      my $xrd = $c->new_xrd;
+      $xrd->link(bar => 'foo');
+      return $xrd;
+    }
+    return;
+  });
+
+my $xrd = $t->app->hostmeta('example.org');
+ok(!$xrd->property, 'Property not found.');
+ok(!$xrd->property('bar'), 'Property not found.');
+is($xrd->at('Link')->attrs('rel'), 'bar', 'Correct link');
+ok(!$xrd->link, 'Empty Link request');
+is($xrd->link('bar')->attrs('href'), 'foo', 'Correct link');
+
+my ($test1, $test2) = (1,1);
+$app->hook(
+  prepare_hostmeta => sub {
+    my ($c, $xrd_ref) = @_;
+    $xrd_ref->property('permanentcheck' => $test1++ );
+  });
+
+$app->hook(
+  before_serving_hostmeta => sub {
+    my ($c, $xrd_ref) = @_;
+    $xrd_ref->property('check' => $test2++ );
+  });
+
+$xrd = $c->hostmeta;
+is($xrd->property('permanentcheck')->text, 1, 'prepare_hostmeta 1');
+is($xrd->property('check')->text, 1, 'before_serving_hostmeta 1');
+
+$xrd = $c->hostmeta;
+is($xrd->property('permanentcheck')->text, 1, 'prepare_hostmeta 2');
+is($xrd->property('check')->text, 2, 'before_serving_hostmeta 2');
+
+$xrd = $c->hostmeta;
+is($xrd->property('permanentcheck')->text, 1, 'prepare_hostmeta 3');
+is($xrd->property('check')->text, 3, 'before_serving_hostmeta 3');
+
+$app->hook(
+  before_serving_hostmeta => sub {
+    my ($c, $xrd_ref) = @_;
+
+    my $link = $xrd_ref->link(salmon => {
+      href => 'http://www.sojolicio.us/'
+    });
+    $link->add('Title' => 'Salmon');
+  });
+
+ok($xrd = $c->hostmeta, 'Get local hostmeta');
+
+ok($xrd->expires, 'Expires exists');
+ok($xrd->at('Expires')->remove, 'Removed Expires');
+
+is_deeply(
+  Mojo::JSON->new->decode($xrd->to_json),
+  {"links" => [
+    {"rel" => "salmon",
+     "titles" => {
+       "default" => "Salmon"
+     },
+     "href" => 'http://www.sojolicio.us/'
+   }
+  ],
+   "properties" => {
+     "permanentcheck" => "1",
+     "check" => "4",
+     "foo" => "bar"
+   }
+ }, 'json Export');
+
+$t->get_ok('/.well-known/host-meta.json')
+    ->status_is(200)
+    ->content_type_is('application/json');
+
+# rel parameter
+$t->get_ok('/.well-known/host-meta?rel=author')
+  ->status_is(200)
+  ->element_exists_not('Link[rel="salmon"]');
+
+ok($xrd = $c->hostmeta, 'Get local HostMeta');
+
+is($xrd->property('permanentcheck')->text, 1, 'Property 1');
+is($xrd->property('foo')->text, 'bar', 'Property 2');
+is($xrd->property('check')->text, 7, 'Property 3');
+is($xrd->link('salmon')->attrs('href'), 'http://www.sojolicio.us/', 'Link 1');
+ok(!$xrd->link('author'), 'Link 2');
+
+ok($xrd = $c->hostmeta(['author']), 'Get local HostMeta');
+
+is($xrd->property('permanentcheck')->text, 1, 'Property 4');
+is($xrd->property('foo')->text, 'bar', 'Property 5');
+is($xrd->property('check')->text, 8, 'Property 6');
+ok(!$xrd->link('salmon'), 'Link 3');
+ok(!$xrd->link('author'), 'Link 4');
+
+$c->hostmeta(
+  sub {
+    my $xrd = shift;
+    is($xrd->property('permanentcheck')->text, 1, 'Property 7');
+    is($xrd->property('check')->text, 9, 'Property 8');
+    is($xrd->link('salmon')->attrs('href'),
+       'http://www.sojolicio.us/', 'Link 5');
+});
+
+$c->hostmeta(
+  ['author'] => sub {
+    my $xrd = shift;
+    is($xrd->property('permanentcheck')->text, 1, 'Property 9');
+    is($xrd->property('check')->text, 10, 'Property 10');
+    ok(!$xrd->link('salmon'), 'Link 6');
+});
+
+$c->hostmeta(
+  undef, ['author'] => sub {
+    my $xrd = shift;
+    is($xrd->property('permanentcheck')->text, 1, 'Property 11');
+    is($xrd->property('check')->text, 11, 'Property 12');
+    ok(!$xrd->link('salmon'), 'Link 7');
+});
+
+pass('No life tests');
+
+done_testing;
+exit;
+
+$c->hostmeta(
+  'undef', ['author'] => sub {
+    my $xrd = shift;
+    ok(!$xrd, 'Not found');
+});
+
+
+is($c->hostmeta('yahoo.com')->subject, 'yahoo.com', 'Title');
+ok(!$c->hostmeta('yahoo.com', -secure), 'Not found for secure');
+
+$c->hostmeta(
+  'yahoo.com' => sub {
+    ok(!$_[0], 'Insecure');
+  } => -secure);
+
+$c->hostmeta(
+  'yahoo.com' => sub {
+    my $xrd = shift;
+    is($xrd->link('hub')->attrs('href'),
+       'http://yhub.yahoo.com',
+       'Correct template');
+    is($xrd->subject, 'yahoo.com', 'Title');
+  });
+
+$c->hostmeta(
+  'e14n.com' => sub {
+    my $xrd = shift;
+    is($xrd->link('lrdd')->attrs('template'),
+       'https://e14n.com/api/lrdd?resource={uri}',
+       'Correct template');
+
+    is($xrd->link('registration_endpoint')->attrs('href'),
+       'https://e14n.com/api/client/register',
+       'Correct template');
+});
+
+$c->hostmeta(
+  'e14n.com' => ['lrdd'] => sub {
+    my $xrd = shift;
+
+    is($xrd->link('lrdd')->attrs('template'),
+       'https://e14n.com/api/lrdd?resource={uri}',
+       'Correct template');
+    ok(!$xrd->link('registration_endpoint'),
+       'no registration endpoint');
+});
+
+
+done_testing;
+exit;
+
+
+__END__
